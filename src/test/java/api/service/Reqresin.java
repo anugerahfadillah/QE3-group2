b94b3e121634155e4af890019049e27e2ef07d59
zatihulwani <zatihulwaani@gmail.com>
2022-04-19 07:51:43 +0700

    add service, stepdef, and schema

---
+++ src/test/java/api/service/Reqresin.java
@@ -0,0 +1,6 @@
+package api.service;
+
+public class Reqresin {
+
+    private static final String REQRESIN_BASEURL = "https://reqres.in";
+}
---
+++ src/test/java/api/stepdef/ReqresinStepDef.java
@@ -0,0 +1,7 @@
+package api.stepdef;
+
+import api.service.Reqresin;
+
+public class ReqresinStepDef {
+    Reqresin reqresinAPI = new Reqresin();
+}
