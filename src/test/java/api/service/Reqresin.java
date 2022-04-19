93b32181b9acfbf30db2e8c835ae6179e69a31ad
Andrian-prayoga <1718054@scholar.itn.ac.id>
2022-04-19 12:14:15 +0700

    update create.feature, stepdef and service

--- src/test/java/api/service/Reqresin.java
+++ src/test/java/api/service/Reqresin.java
@@ -1,6 +1,20 @@
 package api.service;
 
+import net.serenitybdd.rest.SerenityRest;
+import org.json.JSONObject;
+
 public class Reqresin {
 
     private static final String REQRESIN_BASEURL = "https://reqres.in";
+
+    public void postCreate(){
+
+        JSONObject bodyJson = new JSONObject();
+
+        bodyJson.put("name", "morpheus");
+        bodyJson.put("job", "leader");
+        bodyJson.put("updatedAt", "2022-04");
+
+        SerenityRest.given().header("Content-type", "application/json").body(bodyJson.toString()).post(REQRESIN_BASEURL + "/api/users");
+    }
 }
