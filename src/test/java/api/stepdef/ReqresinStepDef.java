7758be6bf0c2555033d007804097b5a39bd11b1c
zatihulwani <zatihulwaani@gmail.com>
2022-04-19 07:55:58 +0700

    add service, stepdef, and schema

--- src/test/java/api/stepdef/ReqresinStepDef.java
+++ src/test/java/api/stepdef/ReqresinStepDef.java
@@ -1,7 +1,21 @@
 package api.stepdef;
 
 import api.service.Reqresin;
+import io.cucumber.java.en.Then;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
 
 public class ReqresinStepDef {
     Reqresin reqresinAPI = new Reqresin();
+
+    //action
+
+
+    //validation
+    @Then("response structure should match json schema {string}")
+    public void validateJsonSchema(String schema) {
+        String path = String.format("schema/%s", schema);
+        restAssuredThat(response -> response.assertThat().body(matchesJsonSchemaInClasspath(path)));
+    }
 }
